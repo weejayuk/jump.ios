d3a010bbf8594fb48f4c4246ba54ddb98e2d5701
Lilli <lilli@janrain.com>
2012-08-02 14:45:04 -0700

    Updated several of the Capture types documentation

--- Janrain/JRCapture/Classes/JRCaptureTypes.h
+++ Janrain/JRCapture/Classes/JRCaptureTypes.h
@@ -34,91 +34,232 @@
  **/
 
 /**
- * TODO
+ * Some types do not perfectly map between Objective-C and Capture. For example, a \e boolean on Capture can have the
+ * values \c true, \c false, and \c null, while an \e NSBoolean in Objective-C can only be \c true or \c false. On the
+ * other hand, an \e NSNumber can represent \c true, \c false, and \c null, but it can also hold many other things
+ * that won't be accepted by the Capture API (such as \c -100) when the property has the type \e boolean.
+ *
+ * To ensure that your objects can correctly update themselves on Capture (i.e., contain \c null), we use an \e NSNumber
+ * to hold \e boolean properties. To make it perfectly clear that \e boolean properties cannot be just any number, we
+ * typedeffed them \e JRBoolean. Your IDE's auto-complete should suggest \e JRBoolean when setting a \e boolean
+ * property, but since underneath it is just an \e NSNumber, you can set it to \c null as well.
+ *
+ * When setting a property of type \e JRBoolean, you can set it as you would an \e NSNumber. Additionally, for your
+ * convenience, Capture objects that have \e boolean properties will provide getters and setters that accept and return
+ * primitive booleans. For example, the JRPhoneNumbersElement#primary property has the following getters/setters:
+ * JRPhoneNumbersElement#primary(), JRPhoneNumbersElement#setPrimary(), JRPhoneNumbersElement#setPrimaryWithBool:(),
+ * and JRPhoneNumbersElement#getPrimaryBoolValue().
+ *
+ * @note
+ *   In Capture there is a difference between a \e boolean with the value \c false and a \e boolean with the value
+ *   \c null.
+ *
+ * @warning
+ *   The compiler won't stop you from setting a \e JRBoolean to a non-boolean value, the JUMP for iOS library will
+ *   automatically convert your \e JRBoolean properties to the equivalent boolean value when updating the object on
+ *   Capture.
+ *
  * @sa
- *   For more information, please see the \ref typesTable "Type Mapping Reference Page"
- **/
-typedef NSString JRString;
-
-/**
- * TODO
- * @sa
- *   For more information, please see the \ref typesTable "Type Mapping Reference Page"
+ *   For more information, please see the \ref typesTable "Type Mapping Reference Page".
  **/
 typedef NSNumber JRBoolean;
 
 /**
- * TODO
+ * Some types do not perfectly map between Objective-C and Capture. For example, a \e decimal on Capture is more or
+ * less equivalent to an \e NSNumber in Objective-C. On the other hand an \e NSNumber can hold values that won't be
+ * accepted by the Capture API (such as \c true).
+ *
+ * To ensure that your objects can correctly update themselves on Capture, we use an \e NSNumber to hold \e decimal
+ * properties. To make it perfectly clear that these properties correspond to the type \e decimal, and they cannot be
+ * just any number, we typedeffed them \e JRDecimal. Your IDE's auto-complete should suggest \e JRDecimal when setting
+ * a \e decimal property. Also, since, underneath, it is just an \e NSNumber, you can set it to \c null as well.
+ *
+ * @warning
+ *   The compiler won't stop you from setting a \e JRDecimal to a value that may not be accepted by Capture, but you
+ *   will get an error when you try and update your object on Capture.
+ *
  * @sa
- *   For more information, please see the \ref typesTable "Type Mapping Reference Page"
+ *   For more information, please see the \ref typesTable "Type Mapping Reference Page".
  **/
 typedef NSNumber JRDecimal;
 
 /**
- * TODO
+ * Some types do not perfectly map between Objective-C and Capture. For example, a \e integer on Capture can hold
+ * both integers and \c null, while an \e NSInteger in Objective-C can only hold integers and can't be \c null. On the
+ * other hand an \e NSNumber can represent \c true, \c false, and \c null, but it can also hold many other things that
+ * won't be accepted by the Capture API (such as \c -100) when the property has the type \e integer.
+ *
+ * To ensure that your objects can correctly update themselves on Capture, we had to use an \e NSNumber to hold
+ * \e integer properties. To make it perfectly clear that \e integer properties cannot be just any number, we
+ * typedeffed them \e JRInteger. Your IDE's auto-complete should suggest \e JRInteger when setting a \e integer
+ * property, but since underneath it is just an \e NSNumber, you can set it to \c null as well.
+ *
+ * When setting a property of type \e JRInteger, you can set it as you would an NSNumber. Additionally, for your
+ * convenience, Capture objects that have \e integer properties will provide getters and setters that accept and return
+ * primitive integers. The same goes for \e JRBooleans. For example, the JRPhoneNumbersElement#primary property
+ * (a \c boolean) has the following getters/setters: JRPhoneNumbersElement#primary(),
+ * JRPhoneNumbersElement#setPrimary(), JRPhoneNumbersElement#setPrimaryWithBool:(), and
+ * JRPhoneNumbersElement#getPrimaryBoolValue().
+ *
+ * @note
+ *   In Capture there is a difference between an \e integer with the value \c 0 and an \g integer with the value
+ *   \c null.
+ *
+ * @warning
+ *   The compiler won't stop you from setting a \e JRInteger to a non-integer value, the JUMP for iOS library will
+ *   automatically convert your \e JRInteger properties to the equivalent integer value when updating the object on
+ *   Capture.
+ *
  * @sa
- *   For more information, please see the \ref typesTable "Type Mapping Reference Page"
+ *   For more information, please see the \ref typesTable "Type Mapping Reference Page".
  **/
 typedef NSNumber JRInteger;
 
 /**
- * TODO
+ * Some types do not perfectly map between Objective-C and Capture. For example, a property of type \e date on Capture
+ * can hold only hold values that correspond to the <a href="http://dotat.at/tmp/ISO_8601-2004_E.pdf">
+ *   ISO 8601 date format</a> (e.g., \c yyyy-MM-dd), while an \e NSDate on Objective-C can hold many more values and
+ * formats.
+ *
+ * To ensure that your objects can correctly update themselves on Capture, we had to use an \e NSDate to hold
+ * \e date properties. To make it perfectly clear that \e date properties cannot be just any date, we typedeffed
+ * them \e JRDate. Your IDE's auto-complete should suggest \e JRDate when setting a \e date property, but since
+ * underneath it is just an \e NSDate, you can set it to \c null as well.
+ *
+ * @note
+ *   In Capture there is a difference between the type \e date and the type \e dateTime. Properties of either type will
+ *   use an \e NSDate to store its value.
+ *
+ * @warning
+ *   The compiler won't stop you from setting a \e JRDate to any valid date value, but the JUMP for iOS library will
+ *   automatically convert your \e JRDate properties to acceptable <a href="http://dotat.at/tmp/ISO_8601-2004_E.pdf">
+ *   ISO 8601 dates</a> (e.g., \c yyyy-MM-dd) when updating the object on Capture.
+ *
  * @sa
- *   For more information, please see the \ref typesTable "Type Mapping Reference Page"
+ *   For more information, please see the \ref typesTable "Type Mapping Reference Page".
  **/
 typedef NSDate   JRDate;
 
 /**
- * TODO
+ * Some types do not perfectly map between Objective-C and Capture. For example, a property of type \e dateTime on
+ * Capture can hold only hold values that correspond to the <a href="http://dotat.at/tmp/ISO_8601-2004_E.pdf">
+ *   ISO 8601 dateTime format</a> (e.g., \c <code>yyyy-MM-dd HH:mm:ss.SSSSSS ZZZ</code>), while an \e NSDate on
+ * Objective-C can hold many more values and formats.
+ *
+ * To ensure that your objects can correctly update themselves on Capture, we had to use an \e NSDate to hold
+ * \e dateTime properties. To make it perfectly clear that \e dateTime properties cannot be just any date, we
+ * typedeffed them \e JRDateTime. Your IDE's auto-complete should suggest \e JRDateTime when setting a \e dateTime
+ * property, but since underneath it is just an \e NSDate, you can set it to \c null as well.
+ *
+ * @note
+ *   In Capture there is a difference between the type \e date and the type \e dateTime. Properties of either type will
+ *   use an \e NSDate to store its value.
+ *
+ * @warning
+ *   The compiler won't stop you from setting a \e JRDateTime to any valid date value, but the JUMP for iOS library
+ *   will automatically convert your \e JRDateTime properties to acceptable
+ *   <a href="http://dotat.at/tmp/ISO_8601-2004_E.pdf">ISO 8601 dateTimes</a>
+ *   (e.g., <code>yyyy-MM-dd HH:mm:ss.SSSSSS ZZZ</code>) when updating the object on Capture.
+ *
  * @sa
- *   For more information, please see the \ref typesTable "Type Mapping Reference Page"
+ *   For more information, please see the \ref typesTable "Type Mapping Reference Page".
  **/
 typedef NSDate   JRDateTime;
 
 /**
- * TODO
+ * Some types do not perfectly map between Objective-C and Capture. For example, a property of type \e ipAddress on
+ * Capture can hold only hold valid IPAddresses (e.g., \c "192.168.0.1") or \c null, where valid IPAddresses are stored
+ * as \e NSStrings in Objective-C.
+ *
+ * To ensure that your objects can correctly update themselves on Capture, we use an \e NSString to hold
+ * \e ipAddress properties. To make it perfectly clear that \e ipAddress properties cannot be just any string, we
+ * typedeffed them \e JRIpAddress. Your IDE's auto-complete should suggest \e JRIpAddress when setting a \e ipAddress
+ * property, but since underneath it is just an \e NSString, you can set it to \c null as well.
+ *
+ * @warning
+ *   The compiler won't stop you from setting a \e JRIpAddress to a value that may not be accepted by Capture, but you
+ *   will get an error when you try and update your object on Capture.
+ *
  * @sa
- *   For more information, please see the \ref typesTable "Type Mapping Reference Page"
+ *   For more information, please see the \ref typesTable "Type Mapping Reference Page".
  **/
 typedef NSString JRIpAddress;
 
 /**
- * TODO
+ * Some types do not perfectly map between Objective-C and Capture. For example, a property of type \e password on
+ * Capture can hold both strings and dictionaries. As such, we use an \e NSObject to hold password properties, though
+ * what is stored in the \e NSObject will either be an \e NSString or \e NSDictionary.
+ *
+ * To ensure that your objects can correctly update themselves on Capture, we use an \e NSObject to hold
+ * \e password properties. To make it perfectly clear that \e password properties can be strings or dictionaries, we
+ * typedeffed them \e JRPassword. Your IDE's auto-complete should suggest \e JRPassword when setting a \e password
+ * property, but since underneath it is just an \e NSObject, you can set it to \c null as well.
+ *
+ * @warning
+ *   The compiler won't stop you from setting a \e JRPassword to a value that may not be accepted by Capture, but you
+ *   will get an error when you try and update your object on Capture.
+ *
  * @sa
- *   For more information, please see the \ref typesTable "Type Mapping Reference Page"
+ *   For more information, please see the \ref typesTable "Type Mapping Reference Page".
  **/
 typedef NSObject JRPassword;
 
 /**
- * Any object hierarchy that JSONKit can serialize. This includes and is limited to NSArray, NSDictionary, NSString,
- * NSNumber, and NSNull.
+ * Some types do not perfectly map between Objective-C and Capture. For example, a property of type \e json on
+ * Capture can hold any valid <a href="http://www.json.org/">JSON</a> object (objects, arrays, strings, numbers,
+ * booleans, or null). When deserializing properties of type \e json, the JUMP for iOS library stores the result in an
+ * \e NSObject property. That is, a \e json property can hold any object hierarchy that JSONKit can serialize.
+ * This includes and is limited to \e NSArray, \e NSDictionary, \e NSString, \e NSNumber, and \e NSNull.
  *
  * For more information on JSONKit and what it can serialize check the README in the project:
  * https://github.com/johnezang/JSONKit
  *
+ * To ensure that your objects can correctly update themselves on Capture, we use an \e NSObject to hold \e json
+ * properties. To make it perfectly clear that \e json properties can only be types that are accepted by JSONKit, we
+ * typedeffed them \e JRJsonObject. Your IDE's auto-complete should suggest \e JRJsonObject when setting a \e json
+ * property, but since underneath it is just an \e NSObject, you can set it to \c null as well.
+ *
+ * @note
+ *   If the JSON returned by Capture is an object, this property will hold an \e NSDictionary. If
+ *   the JSON returned by Capture is an array, this property will hold an \e NSArray. If it is a string, \e NSString,
+ *   number or boolean, \e NSNumber, and if the property is \c null, it will hold \c nil.
+ *
+ * @warning
+ *   The compiler won't stop you from setting a \e JRJsonObject to a value that may not be accepted by Capture or
+ *   JSONKit, but you will get an error when you try and update your object on Capture.
+ *
  * @sa
- *   For more information, please see the \ref typesTable "Type Mapping Reference Page"
- */
+ *   For more information, please see the \ref typesTable "Type Mapping Reference Page".
+ **/
 typedef NSObject JRJsonObject;
 
 /**
- * TODO
+ * 
  * @sa
  *   For more information, please see the \ref typesTable "Type Mapping Reference Page"
  **/
 typedef NSArray  JRStringArray;
 
 /**
- * TODO
+ * Some types do not perfectly map between Objective-C and Capture. For example, a property of type \e uuid on
+ * Capture holds a string and cannot be modified by a client; that is, \e uuid properties are read-only. To make this
+ * clear, the JUMP for iOS library stores the \e uuid property as an \e NSString typedeffed as a \e JRUuid.
+ *
  * @sa
- *   For more information, please see the \ref typesTable "Type Mapping Reference Page"
+ *   For more information, please see the \ref typesTable "Type Mapping Reference Page".
  **/
 typedef NSString JRUuid;
 
 /**
- * TODO
+ * Some types do not perfectly map between Objective-C and Capture. For example, a property of type \e id on
+ * Capture holds a integer id and cannot be modified by a client; that is, \e id properties are read-only. To make this
+ * clear, the JUMP for iOS library stores the \e uuid property as an \e NSNumber typedeffed as a \e JRObjectId.
+ *
+ * @note
+ *   Only the top-level JRCaptureUser and elements of plurals have \e id properties. As there can be several plural
+ *   elements that are similar (
  * @sa
- *   For more information, please see the \ref typesTable "Type Mapping Reference Page"
+ *   For more information, please see the \ref typesTable "Type Mapping Reference Page".
  **/
 typedef NSNumber JRObjectId;
 
@@ -129,11 +270,11 @@ typedef NSNumber JRObjectId;
  * is the same as the JSON \c "String" and the Objective-C foundational class \c "NSString".
  *
  * Some types do not perfectly map between systems. For example, a \c boolean on Capture can have the
- * values \c true, \c false, and \c null, while an \c NSBoolean in Objective-C can only be \c YES or \c NO.
- * On the other hand an \c NSNumber can represent \c true, \c false, and \c null, but it can also hold many other things
+ * values \c true, \c false, and \c null, while an \e NSBoolean in Objective-C can only be \c YES or \c NO.
+ * On the other hand an \e NSNumber can represent \c true, \c false, and \c null, but it can also hold many other things
  * that won't be accepted by the Capture API (such as \c -100).
  *
- * To ensure that your objects can correctly update themselves on Capture, we had to use an \c NSNumber to hold
+ * To ensure that your objects can correctly update themselves on Capture, we had to use an \e NSNumber to hold
  * boolean properties. To make it perfectly clear that boolean properties cannot be just any number, we typedeffed
  * them \c JRBoolean.  Your IDE's auto-complete should suggest JRBoolean when setting a boolean property, but since
  * underneath it is just an NSNumber, you can set it to \c null as well.
@@ -147,18 +288,18 @@ typedef NSNumber JRObjectId;
  * | Schema Type | Json Type        | Obj-C Type | JRCapture Type                  | Recursive  | Typedeffed | Notes|
  * ---------------------------------------------------------------------------------------------------------------------
  * | string      | String           | NSString   | NSString                        |            | No         |      |
- * | boolean     | Boolean          | NSNumber   | ::JRBoolean                     |            | \b Yes     |      |
- * | integer     | Number           | NSNumber   | ::JRInteger                     |            | \b Yes     |      |
+ * | boolean     | Boolean          | NSNumber   | ::JRBoolean                     |            | \e Yes     |      |
+ * | integer     | Number           | NSNumber   | ::JRInteger                     |            | \e Yes     |      |
  * | decimal     | Number           | NSNumber   | NSNumber                        |            | No         |      |
- * | date        | String           | NSDate     | ::JRDate                        |            | \b Yes     |      |
- * | dateTime    | String           | NSDate     | ::JRDateTime                    |            | \b Yes     |      |
- * | ipAddress   | String           | NSString   | ::JRIpAddress                   |            | \b Yes     |      |
- * | password    | String or Object | NSObject   | ::JRPassword                    |            | \b Yes     |      |
- * | JSON        | (any type)       | NSObject   | ::JRJsonObject                  |            | \b Yes     | The JSON type is unstructured data; it only has to be valid parseable JSON. |
- * | plural      | Array            | NSArray    | NSArray or ::JRStringArray      | \b Yes     | No/\b Yes  | Primitive child attributes of plurals may have the constraint locally-unique. |
- * | object      | Object           | NSObject   | JR&lt;<em>PropertyName</em>&gt; | \b Yes     | No         |      |
- * | uuid        | String           | NSString   | ::JRUuid                        |            | \b Yes     | Not an externally usable type. |
- * | id          | Number           | NSNumber   | ::JRObjectId                    |            | \b Yes     | Not an externally usable type. |
+ * | date        | String           | NSDate     | ::JRDate                        |            | \e Yes     |      |
+ * | dateTime    | String           | NSDate     | ::JRDateTime                    |            | \e Yes     |      |
+ * | ipAddress   | String           | NSString   | ::JRIpAddress                   |            | \e Yes     |      |
+ * | password    | String or Object | NSObject   | ::JRPassword                    |            | \e Yes     |      |
+ * | JSON        | (any type)       | NSObject   | ::JRJsonObject                  |            | \e Yes     | The JSON type is unstructured data; it only has to be valid parseable JSON. |
+ * | plural      | Array            | NSArray    | NSArray or ::JRStringArray      | \e Yes     | No/\e Yes  | Primitive child attributes of plurals may have the constraint locally-unique. |
+ * | object      | Object           | NSObject   | JR&lt;<em>PropertyName</em>&gt; | \e Yes     | No         |      |
+ * | uuid        | String           | NSString   | ::JRUuid                        |            | \e Yes     | Not an externally usable type. |
+ * | id          | Number           | NSNumber   | ::JRObjectId                    |            | \e Yes     | Not an externally usable type. |
  * @endtable
  *
  *
