84e3e223f0e348e42283e33aee57e30770c34712
Nathan <nathan@janrain.com>
2012-08-21 16:29:48 -0700

    removed dead JRWebviewWrapper

---
+++ Samples/SimpleCaptureDemo/JRCapture/JRCaptureJsWidgetWrapper.m
@@ -0,0 +1,19 @@
+//
+// Created by nathan on 8/20/12.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "JRCaptureJsWidgetWrapper.h"
+
+
+@implementation JRCaptureJsWidgetWrapper
+
++ (void)startJsWidget
+{
+
+}
+
+
+@end
