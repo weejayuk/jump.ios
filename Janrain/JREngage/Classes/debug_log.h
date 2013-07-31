e03319659dc18881e281da8eed200cc59bd43dd8
Nathan <nathan@janrain.com>
2013-07-30 17:41:03 -0700

    MOB-168 offer quieter logs with warnings squelched

--- Janrain/JREngage/Classes/debug_log.h
+++ Janrain/JREngage/Classes/debug_log.h
@@ -1,12 +1,26 @@
+#import <Foundation/Foundation.h>
+
 #ifndef DLog
   #ifdef DEBUG
     #define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
   #else
-    #define DLog(...)
+    #define DLog(fmt, ...) JRLogExpressionSink((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
   #endif
 #endif
 
-#define ALog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#ifndef ALog
+  #ifdef DEBUG
+    #define ALog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+  #else
+    #ifdef JR_NO_RELEASE_LOGGING
+      #define ALog(fmt, ...) JRLogExpressionSink((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+    #else
+      #define ALog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+    #endif
+  #endif
+#endif
+
+void JRLogExpressionSink(NSString *format, ...);
 
 @interface NSException (JR_raiseDebugException)
 + (void)raiseJRDebugException:(NSString *)name format:(NSString *)format, ...;
--- Janrain/JREngage/Classes/debug_log.m
+++ Janrain/JREngage/Classes/debug_log.m
@@ -1,5 +1,18 @@
 #include "debug_log.h"
 
+// UPS-2039
+void JRLogExpressionSink(NSString *format, ...)
+{
+    va_list va;
+    va_start(va, format);
+    NSString *string = [[NSString alloc] initWithFormat:format arguments:va];
+#if !__has_feature(objc_arc)
+    [string autorelease];
+#endif
+    va_end(va);
+    [string description];
+}
+
 @implementation NSException (JR_raiseDebugException)
 + (void)raiseJRDebugException:(NSString *)name1 format:(NSString *)format, ...
 {
@@ -11,17 +24,20 @@
 
     va_list va;
     va_start(va, format);
+    NSString *string = [[NSString alloc] initWithFormat:format arguments:va];
+#if !__has_feature(objc_arc)
+    [string autorelease];
+#endif
+    va_end(va);
 
     if (debug)
     {
-        [NSException raise:name1 format:format, va];
+        [NSException raise:name1 format:@"%@", string];
     }
     else
     {
-        NSLog((@"%s [Line %d] ", format), __PRETTY_FUNCTION__, __LINE__, va);
+        NSLog(@"%@", string);
     }
-
-    va_end(va);
 }
 
 @end
