f2af624cabbbb2e2b94850d922489c7c9981d0bc
Nathan <nathan@janrain.com>
2013-07-31 11:40:42 -0700

    fixes project file from last merge (missing JRNativeAuth files), fixes minor typo introduced by that merge commit

--- Janrain/JRConnectionManager/NSMutableURLRequest+JRRequestUtils.m
+++ Janrain/JRConnectionManager/NSMutableURLRequest+JRRequestUtils.m
@@ -27,8 +27,8 @@
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-`
-#import "NSMutableURLRequest+JRRequestUtils.h"
+
+ #import "NSMutableURLRequest+JRRequestUtils.h"
 #import "NSDictionary+JRQueryParams.h"
 #import "debug_log.h"
 
