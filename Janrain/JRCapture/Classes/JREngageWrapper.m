cd81f73bed34c0f6dd222e7c038737385f6598c1
Nathan <nathan@janrain.com>
2013-08-12 13:11:48 -0700

    adds additional debug logging for debugging UI state transitions

--- Janrain/JRCapture/Classes/JREngageWrapper.m
+++ Janrain/JRCapture/Classes/JREngageWrapper.m
@@ -63,9 +63,8 @@ static JREngageWrapper *singleton = nil;
 - (JREngageWrapper *)init
 {
     if ((self = [super init])) {
-        [[NSNotificationCenter defaultCenter]
-            addObserver:self selector:@selector(tearingDownViewControllers:)
-                   name:@"JRTearingDownViewControllers" object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(tearingDownViewControllers:)
+                                                     name:@"JRTearingDownViewControllers" object:nil];
 
         self.didTearDownViewControllers = NO;
 
@@ -181,18 +180,20 @@ expandedCustomInterfaceOverrides:(NSMutableDictionary *)expandedCustomInterfaceO
 }
 
 - (void)tearingDownViewControllers:(NSNotification *)notification {
+    DLog();
     self.didTearDownViewControllers = YES;
 }
 
 - (void)engageLibraryTearDown
 {
+    DLog();
     if (self.didTearDownViewControllers) {
+        DLog();
         [JREngage updateTokenUrl:nil];
         self.delegate = nil;
         self.nativeSignInViewController = nil;
         self.engageToken = nil;
         self.didTearDownViewControllers = NO;
-
     }
 }
 
